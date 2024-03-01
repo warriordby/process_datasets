f1387d17d542cb1fd317eb2a3ada6cf3955ee073
warriordby <138351974+warriordby@users.noreply.github.com>
2024-03-01 18:35:16 +0800

    Create Finalcheck.py

---
+++ Finalcheck.py
@@ -0,0 +1,18 @@
+from scipy.io import loadmat
+
+
+mat_file_path = r'C:\Users\86156\Desktop\program\Git\test\DUTLF-FS\TrainingSet\FS(mat)\0001.mat ' 
+mat_data = loadmat(mat_file_path)
+
+for key, value in mat_data.items():
+    if '__' not in key: 
+        print(f"Key: {key}")
+        print(f"Value: {value.shape}\n")
+
+from PIL import Image
+import numpy as np
+
+first_image = mat_data['images'][1]
+first_image = Image.fromarray(np.uint8(first_image))
+image_file_path = r'C:\Users\86156\Desktop\program\Git\test.jpg'
+first_image.save(image_file_path)
